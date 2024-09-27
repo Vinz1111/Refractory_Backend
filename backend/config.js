c564b3734631c7be9588f59220297db04581d8c3
Vinz1111 <vinzent.hardt@outlook.com>
2024-09-27 17:51:02 +0200

    alte Datenbank verbinndung

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,6 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb://localhost:27017/sections-list3';
-
-
 //export const PORT = 5555;
-//export const mongoDBURL = 'mongodb+srv://Vinzent:mLncIXXOzT1jn1hd@cluster0.n2nya.mongodb.net/sections-list3?retryWrites=true&w=majority&appName=Cluster0';
+//export const mongoDBURL = 'mongodb://localhost:27017/sections-list3';
+
+
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://Vinzent:mLncIXXOzT1jn1hd@cluster0.n2nya.mongodb.net/sections-list3?retryWrites=true&w=majority&appName=Cluster0';
